b3200343aa46f40fba40e5c5f15b2bcf24df083b
DaryaCherniak <darya_chernyak@inbox.ru>
2016-09-15 15:52:13 +0300

    com

---
+++ EGH01/EGH01/Controllers/EGHGEAController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EGH01.Controllers
+{
+    public class EGHGEAController : Controller
+    {
+        //
+        // GET: /EGHGEA/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ EGH01/EGH01/Controllers/EGHRGEController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EGH01.Controllers
+{
+    public class EGHRGEController : Controller
+    {
+        //
+        // GET: /EGHRGE/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
