662abc3a7a5720462792d6468c7bf2c16641e803
DaryaCherniak <darya_chernyak@inbox.ru>
2016-09-16 11:14:58 +0300

    commit

---
+++ EGH01/EGH01DB/DB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EGH01DB
+{
+    internal  class DB
+    {
+        static public  SqlConnection Connect()
+        {
+
+            SqlConnection con = null;
+            var s = ConfigurationManager.ConnectionStrings["EGH"];
+            if (s != null)
+            {
+                try
+                {
+                    con = new SqlConnection(s.ConnectionString);
+                    con.Open();
+                }
+                catch (Exception)
+                {
+                    con = null;
+                }
+            }
+            return con;
+        }
+    }
+}
