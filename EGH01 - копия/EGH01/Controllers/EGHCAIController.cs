4c131a0da4cdfd4bc016fe27a41b2b526d5bf7b8
DaryaCherniak <darya_chernyak@inbox.ru>
2016-09-19 17:56:55 +0300

    smw60

---
+++ EGH01 - копия/EGH01/Controllers/EGHCAIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EGH01.Controllers
+{
+    public class EGHCAIController : Controller
+    {
+        EGH01DB.CAIContext db = new EGH01DB.CAIContext(); 
+
+        public ActionResult Index()
+        {
+
+            if (db.IsConnect) ViewBag.msg = "соединение  c БД установлено";
+            else ViewBag.msg = "соединение  c БД  не установлено";
+
+            return View();
+        }
+
+	}
+}
+
+
+;
---
+++ EGH01 - копия/EGH01DB/DB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EGH01DB
+{
+    internal  class DB
+    {
+        static SqlConnection con = null;
+        static public  SqlConnection Connect()
+        {
+            var s = ConfigurationManager.ConnectionStrings["EGH"];
+            if (s != null)
+            {
+                try
+                {
+                    con = new SqlConnection(s.ConnectionString);
+                    con.Open();
+                }
+                catch (Exception)
+                {
+                    con = null;
+                }
+            }
+            return con;
+        }
+        //static public  void Diconnect()
+        //{
+        //    if (con != null) con.Close();  
+        //}
+
+    }
+}
