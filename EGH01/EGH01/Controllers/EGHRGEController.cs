22540e18c48974204246cabd64a1bdae4311e17a
DaryaCherniak <darya_chernyak@inbox.ru>
2016-09-20 23:27:29 +0300

    EGHRGE

--- EGH01/EGH01/Controllers/EGHRGEController.cs
+++ EGH01/EGH01/Controllers/EGHRGEController.cs
@@ -6,21 +6,31 @@ using System.Web.Mvc;
 
 namespace EGH01.Controllers
 {
-   
     public class EGHRGEController : Controller
     {
+        public class InputDate
+        {
+            public DateTime DateIncident { get; set; }
+            public DateTime DateMessage { get; set; }
+            public int volumeNNP { get; set; }
+            public string TypeNNP { get; set; }
+            public int Temp { get; set; }
+            public int Humidity { get; set; }
+            public List<String> TypeInccident = new List<string> { "Нефть", "Резервуар Наземный", "Подземный резервуар", "Авто Транспорт", "Железнодорожный Транспорт" };
+            public List<String> TypeNNP2 = new List<string> {"Нефть", "Бензин", "Мазута" };
+            public List<Object> TypeObj = new List<Object> { "Река", "Лес", "Болото" };
+
+            public DateTime DateRеportWriting { get; set; }
+            public int AreaLand {get; set;}
+        }
         EGH01DB.RGEContext db = new EGH01DB.RGEContext(); 
-        
-        //написать фильтр на открытие БД
+              //написать фильтр на открытие БД
         public ActionResult Index()
         {
             if (db.IsConnect) ViewBag.msg = "соединение  c БД установлено";
             else ViewBag.msg = "соединение  c БД  не установлено";
-
-           
-
-
-            return View();
+            InputDate InputDate =new InputDate();
+            return View(InputDate);
         }
         
         public ActionResult Report()
